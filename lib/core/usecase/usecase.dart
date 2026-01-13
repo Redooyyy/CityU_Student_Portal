2a4dc26067433babb20f09e5e374462386b0c61d
Redooyyy <rwarieal@gmail.com>
2026-01-13 20:56:40 +0600

    created usecase for better controll of single responsibility

---
+++ lib/core/usecase/usecase.dart
@@ -0,0 +1,6 @@
+import 'package:cityu_student_protal/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class UseCase<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(Params params);
+}
